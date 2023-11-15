e66b6228a74925145972e1bd9acf63cce458f684
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-15 13:24:47 +0600

    all menu data queried from database

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", async (req, res) => {
@@ -27,17 +32,16 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-
     // databse & collection
-
-
+    const menuCollection = client.db("bistroBossDB").collection("menu");
 
     // api endpoints
+    app.get("/api/v1/menu", async (req, res) => {
+      const cursor = menuCollection.find();
+      const result = await cursor.toArray();
 
-
-
-
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
